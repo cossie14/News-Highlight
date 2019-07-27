0e5d2a257bf9f2e37ff8e54b5d6bbfc6327492da
sylviah <sylviah.rutto@gmail.com>
2019-07-27 13:35:02 +0300

    added files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_artices
+from .request import get_articles
 
 
 # Views
@@ -11,8 +11,9 @@ def index():
     View root page function that returns the index page and its data
     '''
     popular_articles = get_articles('popular')
-    print(popular_articles)
+    print(trending_articles)(trending)
     title = 'Home - Welcome to the best News Highlight Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html',articles=article, trending = trending)
+
 
  
